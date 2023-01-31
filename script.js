c9179e62e35acfe763968c33c38ee04e7e8d449e
Rohit Rai <66778858+GabbarVirus@users.noreply.github.com>
2023-01-31 15:22:48 +0530

    palindrome checker

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+    let i =0; 
+    let j = str.length-1;
+    str=str.toLowerCase();
+    
+    while(i<j){
+        if(str.charAt(i) == str.charAt(j)){
+            i++;
+            j--;
+        }else{
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
