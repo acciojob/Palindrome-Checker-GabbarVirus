632bb1af4df193d893d5bd558430bab6f99a52e6
Rohit Rai <66778858+GabbarVirus@users.noreply.github.com>
2023-01-31 15:25:35 +0530

    palindrome checker

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // complete the given function
-
 function palindrome(str){
-    let i =0; 
-    let j = str.length-1;
-    str=str.toLowerCase();
-    
-    while(i<j){
-        if(str.charAt(i) == str.charAt(j)){
-            i++;
-            j--;
-        }else{
-            return false;
-        }
-    }
-    return true;
-}
-module.exports = palindrome
+    var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+   
+     /* reverse removeChar for comparison*/
+     var checkPalindrome = removeChar.split('').reverse().join('');
+   
+     /* Check to see if str is a Palindrome*/
+      if(removeChar === checkPalindrome){
+        return true;
+      }else{
+        return false;
+      }
+   }
+   
+   
+   module.exports = palindrome
